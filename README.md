f3c9f17673230ed2d841940add5c501375aecf98
Sofía Celi <sceli@brave.com>
2023-03-22 16:31:25 +0000

    Better readme and benching

--- README.md
+++ README.md
@@ -8,3 +8,48 @@ but adapted to work with Curve25519/Ristretto.
 
 Note that Feldman's scheme reveals information about the secret to dealer with
 unlimited computing power.
+
+*Warning*: This code is a research prototype. Do not use it in production.
+
+## Requirements
+
+In order to [natively](#native) build, run, test and benchmark the library, you will need the following:
+
+```
+  Rust >= 1.61.0
+  Cargo
+```
+
+## Quickstart
+
+### Local
+
+#### Building
+
+To install the latest version of Rust, use the following command (you can also check how to install on the [Rust documentation](https://www.rust-lang.org/tools/install)):
+
+```
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+```
+
+To build the library, run:
+
+```
+  cargo build
+```
+
+#### Testing
+
+To run the tests:
+
+```
+  cargo test
+```
+
+#### Benchmarking
+
+To run a specific set of benchmarks, run (note the this process can slow.):
+
+```
+  cargo bench
+```
--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -848,6 +848,64 @@ fn feldman_sec_1280(c: &mut Criterion) {
     });
 }
 
+fn feldman_sec_1536(c: &mut Criterion) {
+    let mut secret: Secp256k1Scalar = Secp256k1Scalar::new_random();
+    let mut vss = VerifiableSecretSharing {
+        threshold: 10,
+        share_amount: 1536,
+    };
+    let (mut shares, mut commitments) = vss.split(&secret);
+    let mut sub_shares = &shares[0..10];
+    let mut recovered = vss.recover(&sub_shares);
+    c.bench_function("verify_secret", |b| {
+        b.iter(|| VerifiableSecretSharing::verify_all(black_box(shares.as_slice()), &commitments))
+    });
+
+    vss = VerifiableSecretSharing {
+        threshold: 25,
+        share_amount: 1536,
+    };
+    (shares, commitments) = vss.split(&secret);
+    sub_shares = &shares[0..25];
+    recovered = vss.recover(&sub_shares);
+    c.bench_function("verify_secret", |b| {
+        b.iter(|| VerifiableSecretSharing::verify_all(black_box(shares.as_slice()), &commitments))
+    });
+
+    vss = VerifiableSecretSharing {
+        threshold: 50,
+        share_amount: 1536,
+    };
+    (shares, commitments) = vss.split(&secret);
+    sub_shares = &shares[0..50];
+    recovered = vss.recover(&sub_shares);
+    c.bench_function("verify_secret", |b| {
+        b.iter(|| VerifiableSecretSharing::verify_all(black_box(shares.as_slice()), &commitments))
+    });
+
+    vss = VerifiableSecretSharing {
+        threshold: 100,
+        share_amount: 1536,
+    };
+    let (shares, commitments) = vss.split(&secret);
+    let sub_shares = &shares[0..100];
+    let recovered = vss.recover(&sub_shares);
+    c.bench_function("verify_secret", |b| {
+        b.iter(|| VerifiableSecretSharing::verify_all(black_box(shares.as_slice()), &commitments))
+    });
+
+    vss = VerifiableSecretSharing {
+        threshold: 128,
+        share_amount: 1536,
+    };
+    let (shares, commitments) = vss.split(&secret);
+    let sub_shares = &shares[0..128];
+    let recovered = vss.recover(&sub_shares);
+    c.bench_function("verify_secret", |b| {
+        b.iter(|| VerifiableSecretSharing::verify_all(black_box(shares.as_slice()), &commitments))
+    });
+}
+
 fn feldman_sec_1792(c: &mut Criterion) {
     let mut secret: Secp256k1Scalar = Secp256k1Scalar::new_random();
     let mut vss = VerifiableSecretSharing {
@@ -906,6 +964,64 @@ fn feldman_sec_1792(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, feldman_sec_1792); // change to the appropriate parameter
+fn feldman_sec_2048(c: &mut Criterion) {
+    let mut secret: Secp256k1Scalar = Secp256k1Scalar::new_random();
+    let mut vss = VerifiableSecretSharing {
+        threshold: 10,
+        share_amount: 2048,
+    };
+    let (mut shares, mut commitments) = vss.split(&secret);
+    let mut sub_shares = &shares[0..10];
+    let mut recovered = vss.recover(&sub_shares);
+    c.bench_function("verify_secret", |b| {
+        b.iter(|| VerifiableSecretSharing::verify_all(black_box(shares.as_slice()), &commitments))
+    });
+
+    vss = VerifiableSecretSharing {
+        threshold: 25,
+        share_amount: 2048,
+    };
+    (shares, commitments) = vss.split(&secret);
+    sub_shares = &shares[0..25];
+    recovered = vss.recover(&sub_shares);
+    c.bench_function("verify_secret", |b| {
+        b.iter(|| VerifiableSecretSharing::verify_all(black_box(shares.as_slice()), &commitments))
+    });
+
+    vss = VerifiableSecretSharing {
+        threshold: 50,
+        share_amount: 2048,
+    };
+    (shares, commitments) = vss.split(&secret);
+    sub_shares = &shares[0..50];
+    recovered = vss.recover(&sub_shares);
+    c.bench_function("verify_secret", |b| {
+        b.iter(|| VerifiableSecretSharing::verify_all(black_box(shares.as_slice()), &commitments))
+    });
+
+    vss = VerifiableSecretSharing {
+        threshold: 100,
+        share_amount: 2048,
+    };
+    let (shares, commitments) = vss.split(&secret);
+    let sub_shares = &shares[0..100];
+    let recovered = vss.recover(&sub_shares);
+    c.bench_function("verify_secret", |b| {
+        b.iter(|| VerifiableSecretSharing::verify_all(black_box(shares.as_slice()), &commitments))
+    });
+
+    vss = VerifiableSecretSharing {
+        threshold: 128,
+        share_amount: 2048,
+    };
+    let (shares, commitments) = vss.split(&secret);
+    let sub_shares = &shares[0..128];
+    let recovered = vss.recover(&sub_shares);
+    c.bench_function("verify_secret", |b| {
+        b.iter(|| VerifiableSecretSharing::verify_all(black_box(shares.as_slice()), &commitments))
+    });
+}
+
+criterion_group!(benches, feldman_sec_2048); // change to the appropriate parameter
 
 criterion_main!(benches);
